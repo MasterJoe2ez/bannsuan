43903f5422b2d4e313ece5eef33fd2f40873d79d
unknown <jojozaice@gmail.com>
2021-11-20 15:46:25 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import Dashboard from "./Dashboard";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
+
+export default App;
